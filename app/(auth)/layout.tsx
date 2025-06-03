64127ed9ba20d5cbe89862556d62fae163c349cd
Hyejeong <thswjd0609@gmail.com>
2025-06-03 16:34:26 +0900

    fix: (auth) layout 수정

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,25 +6,18 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='min-h-screen flex flex-col'>
-      {/* 상단 헤더 스타일 동일하게 */}
+    <div className='flex h-full'>
       <header className='absolute border-b w-full'>
         <div className='container mx-auto px-2 py-5'>
-          <div className='flex items-center'>
-            <Link
-              href='/'
-              className='text-2xl font-bold text-gray-900 hover:text-point transition-colors'
-            >
-              Hanaro Tech
-            </Link>
-          </div>
+          <Link
+            href='/'
+            className='text-2xl font-bold text-gray-900 hover:text-point transition-colors'
+          >
+            Hanaro Tech
+          </Link>
         </div>
       </header>
-
-      {/* 로그인 콘텐츠 영역 */}
-      <main className='flex flex-1 items-center justify-center px-4'>
-        <div className='w-full max-w-md'>{children}</div>
-      </main>
+      <div className='flex flex-1 items-center justify-center'>{children}</div>
     </div>
   );
 }
